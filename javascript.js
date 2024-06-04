d45b0f3725c53cc3cffe9879a9c9a691e219c0ce
Chris Cooley <ccooley@bonadmin.com>
2024-06-04 11:57:50 -0600

    Add function to take in arrays as input

--- javascript.js
+++ javascript.js
@@ -1,6 +1,11 @@
-function filterWater() {
+function filterWater(contents, filterOut) {
+    const filteredContents = contents.filter(item => !filterOut.includes(item));
+    return filteredContents;
+}
+
+function displayFilteredWater() {
     const contents = ["salt", "dirt", "calcite", "uranium", "rubber"];
     const filterOut = ["uranium", "dirt"];
-    const filteredContents = contents.filter(item => !filterOut.includes(item));
+    const filteredContents = filterWater(contents, filterOut);
     document.getElementById('result').innerHTML = filteredContents.join(', ');
 }
